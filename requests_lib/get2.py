83b45cf6da3e7f81fc6544d68177e0db8470eb20
wkdcjdfyd <64347947+wkdcjdfyd@users.noreply.github.com>
2021-01-20 19:16:16 +0900

    get

---
+++ requests_lib/get2.py
@@ -0,0 +1,19 @@
+import requests
+
+url  = 'https://news.naver.com/main/list.nhn?'
+
+requests_headers = {
+    'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+     '(KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'),
+}
+
+requests_params = {      # 네이버뉴스 > 세계 > 아사아/호주 > 날짜 > 1페이지
+    'sid1' : '104',
+    'sid2' : '231',
+    'date' : '20210120',
+    'page' : '1'
+}
+
+res = requests.get(url, headers = requests_headers, params = requests_params)
+
+print(res)
