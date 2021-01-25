2ad25dc1055ea09f29e2f85b257dadeea697df90
wkdcjdfyd <64347947+wkdcjdfyd@users.noreply.github.com>
2021-01-25 14:38:31 +0900

    update

--- prac_api/company_api2.py
+++ prac_api/company_api2.py
@@ -11,5 +11,12 @@ api_params: dict = {
 res = requests.get(base_url+api_url, params = api_params)
 data = json.loads(res.text)
 
-for x in data['Company']:
-    print(x)
+print(type(res.text))
+print(type(data))
+
+print(res.text)
+print('\n')
+print(data)
+
+# for x in data['Company']:
+#    print(x)
